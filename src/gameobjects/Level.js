387ba69bac8d1c617d0ce4061b6abe1f8eba6a09
Rogelio E. Cardona-Rivera <mail@rogel.io>
2013-11-04 19:00:49 -0500

    Refactored the World creation to its own game object, Level.js

---
+++ src/gameobjects/Level.js
@@ -0,0 +1,140 @@
+
+function Level(level_data) {
+	
+	/* These correspond to the tile (flat) representation of the level.*/
+	var tile_map;
+	var level_tiles;
+	
+	/* These correspond to the block (orthographic) representation of the level.*/
+	var block_map;
+	var level_blocks;
+	
+	/* Class attributes */
+	this.isDisplayingFlat = true; // Are we displaying flat?
+	this.level_data = level_data;
+	
+	/* Class initialization */
+	setup(level_data);
+	
+	
+	this.update = function() {
+		
+	}
+
+	this.draw = function() {
+		if(this.isDisplayingFlat) {
+			// draw flat
+			level_tiles.draw();
+		}
+		
+		else {
+			// draw block (orthographic)
+			level_blocks_draw();
+		}
+	}
+	
+	this.toggleDisplayType = function() {
+		this.isDisplayingFlat = !(this.isDisplayingFlat);
+	}
+	
+	function setup(level_data) {
+		var cell_width = jaws.TileMap.prototype.default_options.cell_size[0]; // 32
+		var cell_height = jaws.TileMap.prototype.default_options.cell_size[1]; // 32
+		
+		var canvas_width = jaws.width; // 768
+		var canvas_height = jaws.height; // 576
+		
+		var num_of_horiz_cells = canvas_width/cell_width; // 24
+		var num_of_vert_cells = canvas_height/cell_height; // 18
+	
+		/* Flat world setup */	
+		tile_map = new jaws.TileMap({
+			size : [num_of_horiz_cells, num_of_vert_cells],
+			cell_size : [cell_width, cell_height]
+		});
+	
+		level_tiles = setup_level_tiles(level_data, num_of_vert_cells, num_of_horiz_cells, cell_width, cell_height);
+		tile_map.push(level_tiles);
+		
+		/* Block (orthographic) world setup */
+		block_map = new jaws.TileMap({
+			size : [num_of_horiz_cells, num_of_vert_cells],
+			cell_size : [cell_width, cell_height]
+		});
+		
+		// level_blocks = setup_level_blocks(level_blocks, num_of_vert_cells, num_of_horiz_cells, cell_width, cell_height);
+		// block_map.push(level_blocks);
+		
+		console.log("Level.js: setup complete");
+	}
+	
+	function setup_level_tiles(level_data, max_rows, max_cols, tile_width, tile_height) {
+		var lvl_tiles = new jaws.SpriteList();
+		for(var row_idx = 0; row_idx < max_rows; row_idx++) {
+			for(var col_idx = 0; col_idx < max_cols; col_idx++) {
+				var data = level_data[row_idx][col_idx];
+				var imgString;
+				if(data != -1) 
+				{
+					if(data >= 60) {//a staircase tile
+						//TODO: IMPLEMENT
+					}
+					
+					else if(data >= 50) {//a battery tile
+						//TODO: IMPLEMENT
+					}
+					
+					else if(data >= 40) {//an end tile
+						imgString = "./assets/art/GoalTile.png";						
+					}
+					
+					else if(data >= 30) {//a start tile
+						imgString = "./assets/art/StartTile.png";
+					}
+					
+					else if(data == 4) {//a gap tile
+						imgString = "./assets/art/TileGap.png";
+					}
+					
+					else if(data == 3) {//a level 3 block
+						imgString = "./assets/art/Level3Tile.png";						
+					}
+					
+					else if(data == 2) {//a level 2 block
+						imgString = "./assets/art/Level2Tile.png";
+					}
+					
+					else if(data == 1) {//a level 1 block
+						imgString = "./assets/art/Level1Tile.png";
+					}
+					
+					else if(data == 0) {//a level 0 block
+						imgString = "./assets/art/Level0Tile.png";	
+					}
+					
+					else {
+						//ERROR
+						//TODO: Do proper error handling
+					}
+					
+					var tile = new jaws.Sprite({
+						image : imgString,
+						x : col_idx * tile_width,
+						y : row_idx * tile_height
+					});
+					
+					console.log(tile); 				
+	
+					lvl_tiles.push(tile);
+				}
+			}
+		}
+		
+		return lvl_tiles;	
+	}
+	
+	function setup_level_blocks(level_data, max_rows, max_cols, tile_width, tile_height) {
+		
+	}
+
+}
--- src/stages/WorldStage.js
+++ src/stages/WorldStage.js
@@ -7,31 +7,17 @@ function WorldStage() {
 	var background;
 	var level_tiles;
 	var tile_map;
+	var active_level;
 	
 	this.setup = function(playerReference) {
 		
-		var cell_width = jaws.TileMap.prototype.default_options.cell_size[0]; //32
-		var cell_height = jaws.TileMap.prototype.default_options.cell_size[1];//32
-		var canvas_width = jaws.width; //768
-		var canvas_height = jaws.height; //576
-		var number_of_horizontal_cells = canvas_width/cell_width; //24
-		var number_of_vertical_cells = canvas_height/cell_height; //18
 		var level_data = setup_sample_level();
 			
-		// A tilemap, each cell is 32x32 pixels. There's 24 such cells across and 18 downwards.
-		tile_map = new jaws.TileMap({
-			size : [number_of_horizontal_cells, number_of_vertical_cells],
-			cell_size : [cell_width, cell_height]
-		});
-		
-		var img_scale_factor = 32/42;
-		
-		//level_data, max_rows, max_cols, tile_width, tile_height, tile_img_scale_factor
-		level_tiles = setup_level_tiles(level_data, number_of_vertical_cells, number_of_horizontal_cells, cell_width, cell_height);
-		tile_map.push(level_tiles);
-
 		// Player setup.
 		player = playerReference;
+		
+		// Level setup.
+		active_level = new Level(level_data);
 		
 		// To quit, press 'esc'
 		jaws.on_keydown("esc", function() {
@@ -52,7 +38,7 @@ function WorldStage() {
 
 	this.draw = function() {
 		jaws.context.clearRect(0, 0, jaws.width, jaws.height);
-		level_tiles.draw();
+		active_level.draw();
 		player.draw();
 		// will call draw() on all items in the list
 	}
@@ -79,75 +65,6 @@ function WorldStage() {
 							[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]];
 		
 		return sample_level;
-	}
-	
-	function setup_level_tiles(level_data, max_rows, max_cols, tile_width, tile_height) {
-		var lvl_tiles = new jaws.SpriteList();
-		for(var row_idx = 0; row_idx < max_rows; row_idx++) {
-			for(var col_idx = 0; col_idx < max_cols; col_idx++) {
-				var data = level_data[row_idx][col_idx];
-				var imgString;
-				if(data != -1) 
-				{
-					if(data >= 60) {//a staircase tile
-						//TODO: IMPLEMENT
-					}
-					
-					else if(data >= 50) {//a battery tile
-						//TODO: IMPLEMENT
-					}
-					
-					else if(data >= 40) {//an end tile
-						imgString = "./assets/art/GoalTile.png";						
-					}
-					
-					else if(data >= 30) {//a start tile
-						imgString = "./assets/art/StartTile.png";
-					}
-					
-					else if(data == 4) {//a gap tile
-						imgString = "./assets/art/TileGap.png";
-					}
-					
-					else if(data == 3) {//a level 3 block
-						imgString = "./assets/art/Level3Tile.png";						
-					}
-					
-					else if(data == 2) {//a level 2 block
-						imgString = "./assets/art/Level2Tile.png";
-					}
-					
-					else if(data == 1) {//a level 1 block
-						imgString = "./assets/art/Level1Tile.png";
-					}
-					
-					else if(data == 0) {//a level 0 block
-						imgString = "./assets/art/Level0Tile.png";	
-					}
-					
-					else {
-						//ERROR
-						//TODO: Do proper error handling
-					}
-					
-					var tile = new jaws.Sprite({
-						image : imgString,
-						x : col_idx * tile_width,
-						y : row_idx * tile_height
-					});
-					
-					console.log(tile); 				
-	
-					lvl_tiles.push(tile);
-				}
-			}
-		}
-		
-		return lvl_tiles;
-	}
-	
-	function scale_image() {
-		
-	}
+	}	
 }
 
