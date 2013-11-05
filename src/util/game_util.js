4be150713250c245d0d354d9d98cf613189ed7b5
Rogelio E. Cardona-Rivera <mail@rogel.io>
2013-11-05 14:04:02 -0500

    Added function to create n-dimensional undefined array.  Thanks StackOverflow!

--- src/util/game_util.js
+++ src/util/game_util.js
@@ -17,7 +17,30 @@ function random_sign() {
 /**
  * Returns a number between -1 and 1.
  * Numbers close to 0 are likely.
+ * Returns a number between -1 and 1. Numbers close to 0 are likely.
  */
 function random_sigmoid() {
 	return (Math.random() - Math.random());
+}
+
+/**
+ * Creates an n-dimensional array, as specified by the parameter list. <br>
+ * e.g.
+ * create_array();    ->  new Array() <br>
+ * create_array(2);    -> new Array(2) <br>
+ * create_array(3, 2); -> [new Array(2), new Array(2), new Array(2)] <br>
+ * <br>
+ * Source:
+ * http://stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript
+ */
+function create_array(length) {
+    var arr = new Array(length || 0),
+        i = length;
+
+    if (arguments.length > 1) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        while(i--) arr[length-1 - i] = create_array.apply(this, args);
+    }
+
+    return arr;
 }
