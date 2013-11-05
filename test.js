c11d5ad86ac219fd4bc1d382049dfe01729a0498
Rogelio E. Cardona-Rivera <mail@rogel.io>
2013-11-05 14:05:37 -0500

    Added test.html and test.'s files to quickly test small JS things - the anti diagonal matrix transform works!

---
+++ test.js
@@ -0,0 +1,14 @@
+/**
+ * @author R. E. Cardona-Rivera
+ */
+
+// @test game_util.js#antidiagonal_transform(rectangular_matrix);
+
+function test_antidiagonal_transform() {
+	var test_matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]];
+	var transformed_matrix = antidiagonal_transform(test_matrix);
+
+	for (var i = 0; i < 3; i++) {
+		console.log(transformed_matrix[i][0] + "," + transformed_matrix[i][1] + "," + transformed_matrix[i][2] + "," + transformed_matrix[i][3])
+	}
+}
