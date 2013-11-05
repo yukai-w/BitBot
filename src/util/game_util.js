49b918d70178eb63c808dec6c633412d43ab8564
Rogelio E. Cardona-Rivera <mail@rogel.io>
2013-11-05 14:04:56 -0500

    Made the anti diagonal matrix transformation (originally defined in Level.js) into its own function.

--- src/util/game_util.js
+++ src/util/game_util.js
@@ -15,8 +15,6 @@ function random_sign() {
 }
 
 /**
- * Returns a number between -1 and 1.
- * Numbers close to 0 are likely.
  * Returns a number between -1 and 1. Numbers close to 0 are likely.
  */
 function random_sigmoid() {
@@ -44,3 +42,74 @@ function create_array(length) {
 
     return arr;
 }
+
+
+/**
+ * Returns a matrix which's rows are now composed of the elements along the
+ * antidiagonal of the parameter rectangular matrix.
+ *
+ * For example, if the matrix is as follows,
+ * [
+ * 	[1, 2, 3, 4]
+ * 	[5, 6, 7, 8]
+ *  [9, 10, 11, 12]
+ * ]
+ *
+ * This function returns,
+ * [
+ * 	[1, 5, 2, 9]
+ *  [6, 3, 10, 7]
+ *  [4, 11, 8, 12]
+ * ]
+ *
+ * @param {Object} rectangular_matrix the matrix to be transformed.
+ * @return {Object} the transformed rectangular_matrix
+ */
+function antidiagonal_transform(rectangular_matrix) {
+	
+	if (rectangular_matrix != null) {
+		if (rectangular_matrix.length != null && rectangular_matrix.length > 0) {
+			if (rectangular_matrix[0].length != null && rectangular_matrix[0].length > 0) {
+				var rows = rectangular_matrix.length;
+				var cols = rectangular_matrix[0].length;
+				
+				var t_matrix = create_array(rows, cols);
+				var t_matrix_row_idx = 0;
+				var t_matrix_col_idx = 0;
+				t_matrix[0][0] = 0;
+
+				/**
+				 * Source: http://stackoverflow.com/questions/2112832/traverse-rectangular-matrix-in-diagonal-strips
+				 */
+				for (var antidiagonal = 0; antidiagonal < rows + cols - 1; antidiagonal++) {
+					var offset_from_left = antidiagonal < rows ? 0 : antidiagonal - rows + 1;
+					var limit_from_top = antidiagonal < cols ? 0 : antidiagonal - cols + 1;
+
+					for (var iter_index = antidiagonal - offset_from_left; iter_index >= limit_from_top; iter_index--) {
+						var row_idx = iter_index;
+						var col_idx = antidiagonal - iter_index;
+						
+						t_matrix[t_matrix_row_idx][t_matrix_col_idx] = rectangular_matrix[row_idx][col_idx];
+
+						t_matrix_col_idx = ((t_matrix_col_idx + 1) % cols);
+						if (t_matrix_col_idx == 0) {//it means we've cycled through an entire row
+							t_matrix_row_idx++;
+							//next row please!
+						}
+					}
+				}
+
+				return t_matrix;
+			} else {
+				return rectangular_matrix;
+			}
+
+		} else {
+			return rectangular_matrix;
+		}
+	} else {
+		return null;
+	}	
+
+}
+
