54b287b0d4f48e169ed3dfe8ce7bf3899bdc3547
Ian Coleman <colemip@gmail.com>
2013-11-09 18:57:33 -0500

    Adding function to clear grid on initialization

--- src/util/editor.js
+++ src/util/editor.js
@@ -15,13 +15,15 @@ $(document).ready(function() {
 }); 
 
 /**
- * Using JSON-like class creating since
+ * Using JSON-like class definition since
  * Editor is a singleton
  */
 var Editor = {
 		
-	width: 0,
-	height: 0,
+	width: 0, // counted in number of tiles
+	height: 0, // counted in number of tiles
+	grid: [],
+
 	
 	/**
 	 * Sets up the Editor
@@ -30,6 +32,19 @@ var Editor = {
 	init: function(widthTileCount, heightTileCount) {
 		this.width = widthTileCount;
 		this.height = heightTileCount; 
+		this.clearGrid();
+	},
+	
+	/**
+	 * Sets all tiles to value 0 (empty tile)
+	 */
+	clearGrid: function() {
+		for(var i=0; i < this.height; i++) {
+			this.grid[i] = new Array(this.width);
+			for(var j=0; j < this.width; j++) {
+				this.grid[i][j] = 0;
+			}
+		}
 	},
 	
 	draw: function() {
