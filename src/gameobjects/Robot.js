0d10529c8c749a88b42b696d98c55c9ea54b1ded
Rogelio E. Cardona-Rivera <mail@rogel.io>
2013-11-12 20:20:40 -0500

    Made the Robot rely on a queue of actions for execution.  Inputting commands through the controls adds the commands to the queue, which (in 2 seconds with no input from the player) are dequeued and executed for the final action.

--- src/gameobjects/Robot.js
+++ src/gameobjects/Robot.js
@@ -19,18 +19,55 @@ function Robot(pos, type) {
 	this.isExecuting = false;
 	this.isIdle = true;
 	this.actionQueue = new goog.structs.Queue();
+	var action_queue_size_max = 12; //max 12 actions queued
+	
+	this.millisecondsSpentPlanning = 0.0;
+	var planning_millisecond_threshold = 2000.0; //2 seconds
+	
 	
 	/* Game input attributes */
 	// Prevent the browser from catching the following keys:
 	jaws.preventDefaultKeys(["up", "down", "left", "right"]); 
-
-	
 	
 	this.update = function() {
 		
 		if(this.isPlayerControlled) {
-			handle_player_input(this);
+			
+			if(this.isIdle) {
+				
+				if(handle_player_input(this)) {
+					//when you're idle, and you begin inputting commands, you enter planning mode.
+					this.setMode('planning'); 
+				}
+
+			} else if(this.isPlanning) {
+				
+				//in planning mode, several things could force you to jump into execution mode: 
+				if(this.millisecondsSpentPlanning > planning_millisecond_threshold) {
+					this.setMode('executing'); //you have two seconds to keep inputting commands.
+					this.millisecondsSpentPlanning = 0.0;
+				} else if(this.actionQueue.getCount() == action_queue_size_max) {
+					this.setMode('executing'); //you can't exceed the max number of actions
+					this.millisecondsSpentPlanning = 0.0;
+				} else {
+					this.millisecondsSpentPlanning += jaws.game_loop.tick_duration;
+					
+					if(handle_player_input(this)) {
+						//when you're planning, and you input commands, the planning timer resets
+						this.millisecondsSpentPlanning = 0.0;
+					}
+				}
+			} else { //must be in execution
+				while( ! this.actionQueue.isEmpty()) {
+					var action = this.actionQueue.dequeue();
+					this.executeAction(action);
+				}
+				
+				this.setMode('idle');
+				
+			}
 		}
+		
 		
 		else { // Do AI
 			handle_AI_input(this); //TODO: IMPLEMENT
@@ -42,6 +79,11 @@ function Robot(pos, type) {
 		this.activeSprite.draw();
 	}
 	
+	/**
+	 * Sets this Robot to the parameter mode.
+	 * @param mode a String which represents the mode to switch into.
+	 * (Acceptable values are 'planning', 'executing', and 'idle'; defaults to 'idle' if mode is unrecognized.)
+	 */
 	this.setMode = function(mode) {
 		if(mode == 'planning') {
 			this.isIdle = false;
@@ -58,6 +100,21 @@ function Robot(pos, type) {
 		}
 	}
 	
+	/**
+	 * Executes the parameter action on this Robot.
+	 * @param action the action to execute ('left','right','up', or 'down')
+	 */
+	this.executeAction = function(action) {
+		if(action == 'left') {
+			this.activeSprite.x -= robot_tile_step_offset;
+		} else if (action == 'right') {
+			this.activeSprite.x += robot_tile_step_offset;
+		} else if(action == 'up') {
+			this.activeSprite.y -= robot_tile_step_offset;
+		} else { //action == 'down'
+			this.activeSprite.y += robot_tile_step_offset;
+		} 
+	}	
 	
 	function handle_AI_input(player_AI) {
 		
@@ -65,48 +122,41 @@ function Robot(pos, type) {
 	
 	
 	/**
-	 * Auxiliary function to handle player input if this object
-	 * is player controlled.
+	 * Auxiliary function to handle player input if this object is player controlled.
+	 * Returns true if a key was pressed.
+	 * @param player the player we're tracking - should be an instance of a Robot.
 	 */
 	function handle_player_input(player) {
-		
+		var key_was_pressed = false;
 		if (jaws.pressedWithoutRepeat("left")) {
-			player.activeSprite.x -= robot_tile_step_offset;
+			// player.activeSprite.x -= robot_tile_step_offset;
+			player.actionQueue.enqueue('left');
+			key_was_pressed = true;
 		} else if (jaws.pressedWithoutRepeat("right")) {
-			player.activeSprite.x += robot_tile_step_offset;			
+			// player.activeSprite.x += robot_tile_step_offset;
+			player.actionQueue.enqueue('right');
+			key_was_pressed = true;
 		} else if(jaws.pressedWithoutRepeat("up")) {
-			player.activeSprite.y -= robot_tile_step_offset;
+			// player.activeSprite.y -= robot_tile_step_offset;
+			player.actionQueue.enqueue('up');
+			key_was_pressed = true
 		} else if(jaws.pressedWithoutRepeat("down")) {
-			player.activeSprite.y += robot_tile_step_offset;
+			// player.activeSprite.y += robot_tile_step_offset;
+			player.actionQueue.enqueue('down');
+			key_was_pressed = true;
 		}
 		
-	}
-	
-		
-	/**
-	 * Checks to see if the player has moved from the previous position
-	 * (relative to before/after player input), and resets the walking animation
-	 * for the sprite if the player has not moved.  
-	 *
-	 * @param {Object} previous_position the position map of the player prior to checking input
- 	 * @param {Object} player the player whose sprite animation will be reset
-	 */
-	function if_idle_reset_direction_animation(previous_position, player) {
-		if(previous_position.x == player.sprite.x)
-		{
-			if(player.isLookingLeft && player.animWalkLeft.index != 0) {
-				look_left(player);
-			}
-			
-			else if(player.isLookingRight && player.animWalkRight.index != 0) {
-				look_right(player);
-			}
+		if(key_was_pressed) {
+			console.log("Key was pressed!");
 		}
+		
+		return key_was_pressed;
 	}
-	
-	
 }
 
+/**
+ * An enum of the Robot types, which contains information of image files.
+ */
 Robot.types = {
 	'human_controlled' : {
 		tile_img : "./assets/art/TileRobot.png",
