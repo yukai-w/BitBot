4e21bcc5dbd214371b9eef88cb1ec22b9d61bd5b
Rogelio E. Cardona-Rivera <mail@rogel.io>
2013-11-16 18:17:59 -0500

    Reset the player if they try to move onto an obstacle.  There is no feedback for crashing though, which I'm working on.

--- src/stages/LevelStage.js
+++ src/stages/LevelStage.js
@@ -15,7 +15,34 @@ function LevelStage() {
 
 	this.update = function() {
 		this.activeLevel.update();
-		this.player.update();		
+		this.player.update();
+		
+		var player_x_pos = this.player.sprite.x;
+		var player_y_pos = this.player.sprite.y;
+		var tiles_at_new_player_position = this.activeLevel.tileMap.at(player_x_pos, player_y_pos);
+		
+		//if the player updates and moves to a place where there is no tile,
+		//have the player fall
+		
+		
+		//if the player updates and moves to a place where there is an obstacle tile, 
+		//then revert the move and apply a penalty
+		if(tiles_at_new_player_position != undefined && tiles_at_new_player_position.length > 0) {
+			var tile = tiles_at_new_player_position[0];  //guaranteed to be of length 1
+			if(tile.type == 'obstacle_tile') {
+				this.player.sprite.moveTo(this.player.previousPosition.x, this.player.previousPosition.y);
+				shakescreen(2);
+								
+			}
+			
+			
+			//TODO: Apply penalty
+			
+		}
+		
+		//if the player updates and moves to a place where there is an enemy
+		//robot, then revert the move and apply a penalty
+			
 		this.hud.update();
 	}
 
