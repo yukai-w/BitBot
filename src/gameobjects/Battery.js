ac2d76a515fdade50c79774c55de96356d10de85
Rogelio E. Cardona-Rivera <mail@rogel.io>
2013-11-20 20:58:05 -0500

    Made the Battery actually make a sound when you collide against it.

--- src/gameobjects/Battery.js
+++ src/gameobjects/Battery.js
@@ -1,12 +1,27 @@
 /**
- * A Battery. 
+ * A Battery.
  */
 function Battery(pos) {
-	
+
 	this.x = pos.x;
 	this.y = pos.y;
 	this.drawing_vert_offset = 5;
-	this.sprite = new jaws.Sprite({x:pos.x, y:pos.y-this.drawing_vert_offset, scale:1.60, anchor:"center", image:"./assets/art/Battery.png"});
+	
+	this.useSfx = new Howl({
+		urls : ['./assets/sounds/fx/powerup.mp3'],
+		volume : 0.35
+	});
+	
+	
+	/* Sprite attributes*/
+	this.sprite = new jaws.Sprite({
+		x : pos.x,
+		y : pos.y - this.drawing_vert_offset,
+		scale : 1.60,
+		anchor : "center",
+		image : "./assets/art/Battery.png"
+	});
+	
 	this.shadowSprite = new jaws.Sprite({
 		x : pos.x,
 		y : (pos.y + 6),
@@ -14,18 +29,21 @@ function Battery(pos) {
 		scale : 0.35,
 		image : "./assets/art/Shadow.png"
 	});
-	
+
 	/* Game logic attributes */
-	this.level = 25.0;
-		
+	var battery_power = 25.0;
+
 	this.draw = function() {
 		this.shadowSprite.draw();
 		this.sprite.draw();
 	}
-	
+
 	this.rect = function() {
-		return this.sprite.rect().resizeTo(8,15);
+		return this.sprite.rect().resizeTo(8, 15);
 	}
-	
-	
+
+	this.use = function() {
+		this.useSfx.play();
+		return battery_power;
+	}
 }
