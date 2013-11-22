e74b7cfd72aa8bc77b7a3feb9aff72c8b26d3fcc
Rogelio E. Cardona-Rivera <mail@rogel.io>
2013-11-22 15:14:52 -0500

    Changed both the background image and the way the animation was done.

--- src/states/PlayState.js
+++ src/states/PlayState.js
@@ -5,57 +5,50 @@
  */
 function PlayState() {
 
-	var current_stage;
-	var current_background;
-	var current_background_idx;
-	var do_background_anim;
-	var background_sprites;
+	var background_animation = new jaws.Animation({
+		sprite_sheet : "./assets/art/BitBotTitleLoop-SpriteSheet.png",
+		frame_size : [288, 288],
+		frame_duration : 300, //ms
+		loop : true,
+		orientation : 'right'
+	});
+	
+	var background_sprite = new jaws.Sprite({x : 0, y : 0, scale : 2});
+	background_sprite.setImage(background_animation.frames[0]);
+	var background_overlay = new jaws.Sprite({x : 0, y : 0, color : 'Gray', alpha : 0.85, width : jaws.width, height : jaws.height});
+	this.currentStage = undefined;
+	
 
 	this.setup = function() {
 
-		/* Background setup */
-		var bg_x_pos = jaws.width/2;
-		var bg_y_pos = jaws.height/2;
-		
-		var bg01 = new jaws.Sprite({x : bg_x_pos, y : bg_y_pos, anchor : "center", image : "./assets/art/LevelBackground01.png"});
-		var bg02 = new jaws.Sprite({x : bg_x_pos, y : bg_y_pos, anchor : "center", image : "./assets/art/LevelBackground02.png"});
-		var bg03 = new jaws.Sprite({x : bg_x_pos, y : bg_y_pos, anchor : "center", image : "./assets/art/LevelBackground03.png"});
-		var bg04 = new jaws.Sprite({x : bg_x_pos, y : bg_y_pos, anchor : "center", image : "./assets/art/LevelBackground04.png"});
-		var bg05 = new jaws.Sprite({x : bg_x_pos, y : bg_y_pos, anchor : "center", image : "./assets/art/LevelBackground05.png"});
-		var bg06 = new jaws.Sprite({x : bg_x_pos, y : bg_y_pos, anchor : "center", image : "./assets/art/LevelBackground06.png"});
-		
-		background_sprites = [bg01, bg02, bg03, bg04, bg05, bg06];
-		current_background_idx = 0;
-		current_background = background_sprites[current_background_idx];
-		do_background_anim = true;
-								
-
 		/* Level setup */
-		current_stage = new LevelStage();
+		this.currentStage = new LevelStage();
 		
 	}
 
 	this.update = function() {
 	
-		if(do_background_anim) {
-			
-			do_background_anim = false;
-			current_background_idx = (current_background_idx + 1) % background_sprites.length;
-			current_background = background_sprites[current_background_idx];
-			setTimeout(function(){
-				do_background_anim = true;
-			}, 50); //ms
-		}
+		background_sprite.setImage(background_animation.next());
+		this.currentStage.update();
 		
-		current_stage.update();
+		//if (! this.currentStage.hasBeenBeaten) {
+		// this.currentStage.update();
+		//} else {
+		// 	load the new stage		
+		// }
+		
+		
+		
+		
 		fps.innerHTML = jaws.game_loop.fps;
 	}
 
 	this.draw = function() {
 
 		jaws.context.clearRect(0, 0, jaws.width, jaws.height);
-		current_background.draw();
-		current_stage.draw();
+		background_sprite.draw();
+		background_overlay.draw();
+		this.currentStage.draw();
 	}
 }
 
