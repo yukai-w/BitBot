1658863aa84d5e5570ea87778eef51722c61584a
Rogelio E. Cardona-Rivera <mail@rogel.io>
2013-11-25 00:16:55 -0500

    Removing unused images.

--- main.js
+++ main.js
@@ -6,15 +6,7 @@ function init() {
 	jaws.assets.add("./assets/art/BitBotTitleIntro-SpriteSheet.png");
 	jaws.assets.add("./assets/art/BitBotTitleLoop-SpriteSheet.png");
 	jaws.assets.add("./assets/art/BitBotGameLoop-SpriteSheet.png");
-	
-	
-	jaws.assets.add("./assets/art/LevelBackground01.png");
-	jaws.assets.add("./assets/art/LevelBackground02.png");
-	jaws.assets.add("./assets/art/LevelBackground03.png");
-	jaws.assets.add("./assets/art/LevelBackground04.png");
-	jaws.assets.add("./assets/art/LevelBackground05.png");
-	jaws.assets.add("./assets/art/LevelBackground06.png");
-	
+
 
 	//Tiles
 	jaws.assets.add("./assets/art/Tile.png");
